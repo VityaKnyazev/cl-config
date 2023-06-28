37b4065dd1aeef136efce2f70e3462ea06e5cf9e
Vitya Knyazev <Utopio@tut.by>
2023-06-28 16:39:19 +0300

    Init config server application..

---
+++ src/main/java/ru/clevertec/ecl/knyazev/ConfigApplication.java
@@ -0,0 +1,15 @@
+package ru.clevertec.ecl.knyazev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+}
